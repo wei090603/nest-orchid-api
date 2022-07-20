1c6226d47bbf58cfe7bf700bf3fd8daaddf7c613
wei090603 <1176022890@qq.com>
2022-07-20 22:13:05 +0800

    manager

--- apps/admin/src/manager/manager.service.ts
+++ apps/admin/src/manager/manager.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Manager } from '@libs/db/entity/manager.entity';
-import { getManager, Like, Repository } from 'typeorm';
+import { Like, Repository, In } from 'typeorm';
 import { hashSync } from 'bcryptjs';
 import { CreateManagerDto, FindManagerDto } from './interface';
 import { Roles } from '@libs/db/entity/roles.entity';
@@ -21,47 +21,69 @@ export class ManagerService {
     private readonly resourcesRepository: Repository<Resources>,
   ) {}
 
-  async findAll({ page = 1, limit = 10, ...params }: FindManagerDto) : Promise<PageResult<Manager>> {
+  async findAll(params: FindManagerDto): Promise<PageResult<Manager>> {
+    const {
+      page = 1,
+      limit = 10,
+      account = '',
+      name = '',
+      phone = '',
+    } = params;
     const [list, total] = await this.managerRepository.findAndCount({
       relations: ['roles'],
       skip: limit * (page - 1),
       take: limit,
-      where: { account: Like(`%${params.account}%`), name: Like(`%${params.name}%`), phone: Like(`%${params.phone}%`) },
+      where: {
+        account: Like(`%${account}%`),
+        name: Like(`%${name}%`),
+        phone: Like(`%${phone}%`),
+      },
       order: { id: 'DESC' },
     });
     return { list, total };
   }
 
   async create(data: CreateManagerDto) {
-    const { account, email, name, remark, phone, roles } = data
-    await this.findOneByAccount(account)
+    const { account, email, name, remark, phone, roles } = data;
+    await this.findOneByAccount(account);
     const result = await this.managerRepository.save({
-      account, email, name, remark, phone
+      account,
+      email,
+      name,
+      remark,
+      phone,
     });
-    const role = await this.rolesRepository.findByIds(roles);
+    const role = await this.rolesRepository.findBy({
+      id: In(roles),
+    });
     await this.managerRepository.save({
       id: result.id,
-      roles: role
-    })
+      roles: role,
+    });
     // const rolesList = await getManager().createQueryBuilder(Roles, "roles").where('roles.id IN (:...ids)', { ids: roles }).getMany();
   }
 
   async updated(id: number, data: CreateManagerDto) {
-    const { email, name, phone, remark, roles } = data
-    const role = await this.rolesRepository.findByIds(roles);
+    const { email, name, phone, remark, roles } = data;
+    const role = await this.rolesRepository.findBy({
+      id: In(roles),
+    });
     await this.managerRepository.update(id, {
-      email, name, phone, remark
+      email,
+      name,
+      phone,
+      remark,
     });
     await this.managerRepository.save({
       id,
-      roles: role
-    })
+      roles: role,
+    });
   }
 
   async updateStatus(id: number) {
-    const existing = await this.managerRepository.findOne(id);
+    const existing = await this.managerRepository.findOneBy({ id });
     await this.managerRepository.update(id, {
-      status: !existing.status
+      status: !existing.status,
     });
   }
 
@@ -75,32 +97,38 @@ export class ManagerService {
 
   // 检查账号是否存在
   async findOneByAccount(account: string) {
-    const existing = await this.managerRepository.findOne({ account });;
+    const existing = await this.managerRepository.findOneBy({ account });
     if (existing) throw new ApiException(10400, '用户已存在');
   }
 
   async resources(roles: any[]) {
-    const roleId: number[] = roles.map((item: any) => item.id); 
-    if (roleId.includes(1)) { // 超级管理员
+    const roleId: number[] = roles.map((item: any) => item.id);
+    if (roleId.includes(1)) {
+      // 超级管理员
       const data = await this.resourcesRepository.find({
         where: { type: 'menu' },
         order: {
-          sort: 'ASC'
-        }
-      })
-      return initTree(data)
+          sort: 'ASC',
+        },
+      });
+      return initTree(data);
     } else {
-      const data = await this.rolesRepository.createQueryBuilder('roles')
-      .leftJoinAndSelect('roles.resources', 'resources', "resources.type = 'menu'")
-      .select('roles.id')
-      .addSelect('resources')
-      .where('roles.id IN (:...ids)', { ids: roleId })
-      .orderBy('resources.id', 'DESC')
-      .getMany()
+      const data = await this.rolesRepository
+        .createQueryBuilder('roles')
+        .leftJoinAndSelect(
+          'roles.resources',
+          'resources',
+          "resources.type = 'menu'",
+        )
+        .select('roles.id')
+        .addSelect('resources')
+        .where('roles.id IN (:...ids)', { ids: roleId })
+        .orderBy('resources.id', 'DESC')
+        .getMany();
       const resourcesList = data.map((item: Roles) => item.resources);
       // const resources = resourcesList.reduce((a: any[], b: any) => a.concat(b) ); // 二维数组转一维数组
-      const newArr = resourcesList.flat()
-      return initTree(newArr)
+      const newArr = resourcesList.flat();
+      return initTree(newArr);
     }
   }
 }
